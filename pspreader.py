e49169bb72ede6c55cee04ea3243ea1154be85de
jon <jonnie_schwartz@hotmail.com>
2020-04-28 10:05:10 +1000

    added functionality for different sentence-enders

--- pspreader.py
+++ pspreader.py
@@ -1,6 +1,13 @@
 def pspread(file):
     f = open(file, 'r')
     content = f.read()
-    scontent = content.replace('. ', '. \n')
+    content = content.replace('. ', '. \n')
+    content = content.replace('! ', '! \n')
+    content = content.replace('? ', '? \n')
     f = open(file, 'w')
-    f.write(scontent)
+    f.write(content)
+#pspread('test.txt')
+
+# functionality to add:
+# add support for other filetypes (markup, word) through conditional read statements
+# ?make it a toggleable function. (add a second binary arg as to whether you are switching it on or off. Paragraphs are an issue here)
